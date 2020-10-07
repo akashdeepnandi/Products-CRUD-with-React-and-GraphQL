dfb750c6b9be8cb789f1db0e11e9b1103a7291de
Akashdeep Nandi <adncorwin@gmail.com>
2020-10-07 15:13:28 +0530

    added try/catch to resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,40 +3,56 @@ const { default: Axios } = require("axios");
 const resolvers = {
   Query: {
     products: async () => {
-      const { data } = await Axios.get(
-        "https://5f3b9c9dfff8550016ae56d0.mockapi.io/products"
-      );
-      return data;
+      try {
+        const { data } = await Axios.get(
+          "https://5f3b9c9dfff8550016ae56d0.mockapi.io/products"
+        );
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
     },
     product: async (parent, { id }) => {
-      const { data } = await Axios.get(
-        `https://5f3b9c9dfff8550016ae56d0.mockapi.io/products/${id}`
-      );
-      return data;
+      try {
+        const { data } = await Axios.get(
+          `https://5f3b9c9dfff8550016ae56d0.mockapi.io/products/${id}`
+        );
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
   Mutation: {
     createProduct: async (_, { name, price, material }) => {
-      const { data } = await Axios.post(
-        "https://5f3b9c9dfff8550016ae56d0.mockapi.io/products/",
-        {
-          name,
-          price,
-          material,
-        }
-      );
-      return data;
+      try {
+        const { data } = await Axios.post(
+          "https://5f3b9c9dfff8550016ae56d0.mockapi.io/products/",
+          {
+            name,
+            price,
+            material,
+          }
+        );
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
     },
     editProduct: async (_, { id, name, price, material }) => {
-      const { data } = await Axios.put(
-        `https://5f3b9c9dfff8550016ae56d0.mockapi.io/products/${id}`,
-        {
-          name,
-          price,
-          material,
-        }
-      );
-      return data;
+      try {
+        const { data } = await Axios.put(
+          `https://5f3b9c9dfff8550016ae56d0.mockapi.io/products/${id}`,
+          {
+            name,
+            price,
+            material,
+          }
+        );
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
     },
     removeProduct: async (_, { id }) => {
       try {
